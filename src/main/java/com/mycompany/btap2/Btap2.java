f8618f2cc8f2a43c6f3e7796fcfe62b3fcff6f92
nguyendangQ <quangviplang@gmail.com>
2025-03-13 00:40:26 +0700

    update1

--- src/main/java/com/mycompany/btap2/Btap2.java
+++ src/main/java/com/mycompany/btap2/Btap2.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.btap2;
 
 /**
@@ -9,7 +8,8 @@ package com.mycompany.btap2;
  * @author quang
  */
 public class Btap2 {
-private String userId;
+
+    private String userId;
     private String email;
 
     public Btap2(String userId, String email) {
@@ -17,9 +17,16 @@ private String userId;
         this.email = email;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public void updateEmail(String newEmail) { this.email = newEmail; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void updateEmail(String newEmail) {
+        this.email = newEmail;
+    }
 
 }
